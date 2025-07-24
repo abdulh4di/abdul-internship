e7436c6c7cc63b6ee91d1d820e04949b3ece87af
Abdul Hadi <a.hadi94@hotmail.co.uk>
2025-07-24 19:56:21 +0100

    Dynamic new collections

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -5,10 +5,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/less/navigation";
 import { Navigation } from "swiper/modules";
+import NewCollectionsSkeleton from "../ui/NewCollectionsSkeleton";
 
 export default function NewCollections() {
   const [newCollections, setNewCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   async function fetchNewCollections() {
+    setLoading(true);
     const { data } = await axios.get(
       "https://remote-internship-api-production.up.railway.app/newCollections"
     );
@@ -16,6 +20,7 @@ export default function NewCollections() {
     const collections = data.data;
 
     setNewCollections(collections);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -28,50 +33,53 @@ export default function NewCollections() {
           <h2 className="new-collections__title">New Collections</h2>
 
           <div className="new-collections__body">
-          <Swiper
-            className="custom-swiper"
-            spaceBetween={10}
-            slidesPerView={6}
-            loop
-            navigation
-            modules={[Navigation]}
-            breakpoints={{
-              0: { slidesPerView: 1 },
-              368: { slidesPerView: 1 },
-              768: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
-              1220: { slidesPerView: 4 },
-              1440: { slidesPerView: 5 },
-            }}
-          >
-              {newCollections.map((newCollection, index) => (
-                <SwiperSlide key={index} className="swiper-slide">
-                  <div className="collection-column">
-                    <Link to="/collection" className="collection">
-                      <img src={newCollection.imageLink} alt="" className="collection__img" />
-                      <div className="collection__info">
-                        <h3 className="collection__name">{newCollection.title}</h3>
-                        <div className="collection__stats">
-                          <div className="collection__stat">
-                            <span className="collection__stat__label">Floor</span>
-                            <span className="collection__stat__data">
-                              {(Math.round(newCollection.floor * 100) / 100).toFixed(2)} ETH
-                            </span>
-                          </div>
-                          <div className="collection__stat">
-                            <span className="collection__stat__label">Total Volume</span>
-                            <span className="collection__stat__data">
-                              {newCollection.totalVolume} ETH
-                            </span>
+            <Swiper
+              className="swiper-slide"
+              spaceBetween={16}
+              loop
+              navigation
+              modules={[Navigation]}
+              breakpoints={{
+                0: { slidesPerView: 1 },
+                480: { slidesPerView: 2 },
+                768: { slidesPerView: 3 },
+                1020: { slidesPerView: 4 },
+                1200: { slidesPerView: 5 },
+                1600: { slidesPerView: 6 },
+              }}
+            >
+              {loading
+                ? new Array(7).fill(0).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <NewCollectionsSkeleton />
+                    </SwiperSlide>
+                  ))
+                : newCollections.map((newCollection, index) => (
+                    <SwiperSlide key={index}>
+                      <Link to={`/collection/${newCollection.collectionId}`} className="collection">
+                        <img src={newCollection.imageLink} alt="" className="collection__img" />
+                        <div className="collection__info">
+                          <h3 className="collection__name">{newCollection.title}</h3>
+                          <div className="collection__stats">
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">Floor</span>
+                              <span className="collection__stat__data">
+                                {(Math.round(newCollection.floor * 100) / 100).toFixed(2)} ETH
+                              </span>
+                            </div>
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">Total Volume</span>
+                              <span className="collection__stat__data">
+                                {newCollection.totalVolume} ETH
+                              </span>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  </div>
-                </SwiperSlide>
-              ))}
-          </Swiper>
-            </div>
+                      </Link>
+                    </SwiperSlide>
+                  ))}
+            </Swiper>
+          </div>
         </div>
       </div>
     </section>
