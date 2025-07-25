b9ec775519477d18f4d112a4508ff2da52833bfa
Abdul Hadi <a.hadi94@hotmail.co.uk>
2025-07-25 01:06:33 +0100

    Dynamic popluar collection

--- src/components/home/PopularCollections.jsx
+++ src/components/home/PopularCollections.jsx
@@ -1,39 +1,85 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import PopularSkeleton from "../ui/PopularSkeleton";
 
 export default function PopularCollections() {
+  const [popularCollections, setPopularCollection] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchPopularCollections() {
+    setLoading(true);
+    const { data } = await axios.get(
+      "https://remote-internship-api-production.up.railway.app/popularCollections"
+    );
+    const collections = data.data;
+
+    setPopularCollection(collections);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPopularCollections();
+  }, []);
+
   return (
     <section id="popular-collections">
       <div className="container">
         <div className="row">
           <h2 className="popular-collections__title">Popular Collections</h2>
+
           <div className="popular-collections__body">
-            {new Array(6).fill(0).map((_, index) => (
-              <div className="collection-column" key={index}>
-                <Link to="/collection" className="collection">
-                  <img
-                    src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
-                    alt=""
-                    className="collection__img"
-                  />
-                  <div className="collection__info">
-                    <h3 className="collection__name">Bored Ape Kennel Club</h3>
-                    <div className="collection__stats">
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">Floor</span>
-                        <span className="collection__stat__data">0.46 ETH</span>
-                      </div>
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">
-                          Total Volume
-                        </span>
-                        <span className="collection__stat__data">281K ETH</span>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
+            <Swiper
+              spaceBetween={16}
+              loop
+              navigation
+              modules={[Navigation]}
+              breakpoints={{
+                0: { slidesPerView: 1 },
+                480: { slidesPerView: 2 },
+                768: { slidesPerView: 3 },
+                1020: { slidesPerView: 4 },
+                1200: { slidesPerView: 5 },
+                1600: { slidesPerView: 6 },
+              }}
+            >
+              {loading
+                ? new Array(7).fill(0).map((_, index) => (
+                    <SwiperSlide>
+                      <PopularSkeleton />
+                    </SwiperSlide>
+                  ))
+                : popularCollections.map((popularCollection, index) => (
+                    <SwiperSlide key={index}>
+                      <Link
+                        to={`/collection/${popularCollection.collectionId}`}
+                        className="collection"
+                      >
+                        <img src={popularCollection.imageLink} alt="" className="collection__img" />
+                        <div className="collection__info">
+                          <h3 className="collection__name">{popularCollection.title}</h3>
+                          <div className="collection__stats">
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">Floor</span>
+                              <span className="collection__stat__data">
+                                {(Math.round(popularCollection.floor * 100) / 100).toFixed(2)} ETH
+                              </span>
+                            </div>
+                            <div className="collection__stat">
+                              <span className="collection__stat__label">Total Volume</span>
+                              <span className="collection__stat__data">
+                                {popularCollection.totalVolume} ETH
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </Link>
+                    </SwiperSlide>
+                  ))}
+            </Swiper>
           </div>
         </div>
       </div>
